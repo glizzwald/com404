c757ba429d82d4708ed1ca5bf9977a8c8f8eec6b
Kyle Vaughan <4barrk68@solent.ac.uk>
2019-10-03 15:45:59 +0000

    OR operator

---
+++ 1-basics/3-decision/9-or-operator/bot.py
@@ -0,0 +1,8 @@
+print("Enter the type of adventure")
+adventure_type = input()
+if (adventure_type == "scary") or (adventure_type == "short"):
+    print("Entering the dark forest!")
+if (adventure_type == "safe") or (adventure_type == "long"):
+    print("Taking the safe route!")
+else:
+    ("Not sure which route to take.")
