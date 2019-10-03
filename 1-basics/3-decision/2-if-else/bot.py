af268e52710f87562f79c14aad4e2dee0396d657
Kyle Vaughan <4barrk68@solent.ac.uk>
2019-10-03 13:38:58 +0000

    Else statements for the bot

---
+++ 1-basics/3-decision/2-if-else/bot.py
@@ -0,0 +1,8 @@
+calculation = input("Please enter the activity to be performed.")
+if (calculation == "calculate"):
+    print("Performing calculations...")
+    print()
+    print("Activity completed!")
+else:
+    print("Performing activity...")
+    print("Activity completed")
