4efcff2a93870b84caeb0913904b0630eddc67bb
Kyle Vaughan <4barrk68@solent.ac.uk>
2019-10-03 14:13:22 +0000

    Comparison Operators

---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,8 @@
+first_number = input("Please enter the first number. ")
+second_number = input("Please enter the second number.")
+
+if (second_number<first_number):
+    print("The second number is the smallest.")
+
+else:
+    print("The first number is the smallest.")
